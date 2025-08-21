756eec3a44c5a83cab3c50d083d729c8ce463aa7
kvenkatesh2811 <kenchugunduvenkey@gmail.com>
2025-08-21 18:48:29 +0530

    removed unnecessary import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Member from "./Components/Members/Member";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
